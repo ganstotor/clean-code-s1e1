0ef25b61fd9140594d9285d96c4a6cf258252919
ganstotor <ganstotor@gmail.com>
2024-12-13 17:48:54 +0200

    refactor(basic-3.4): use classes instead of tag selectors

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 //New task list item
 var createNewTaskElement=function(taskString){
   var listItem=document.createElement("li");
+  listItem.className="todo-item";
 
   //input (checkbox)
   var checkBox=document.createElement("input");//checkbx
@@ -33,16 +34,22 @@ var createNewTaskElement=function(taskString){
 
   label.innerText=taskString;
   label.className="task";
+  label.classList.add("label");
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className="input";
   editInput.type="text";
   editInput.className="task";
+  editInput.classList.add("input");
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
   editButton.className="edit";
+  editButton.classList.add("button");
 
   deleteButton.className="delete";
+  deleteButton.classList.add("button");
+  deleteButtonImg.className="delete-img";
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.alt = "Button to delete todo item";
   deleteButton.appendChild(deleteButtonImg);
